1c578830435132a68da8dd90cc96ed55765bf57a
Slasshy Overhere <125544253+SlasshyOverhere@users.noreply.github.com>
2023-03-17 01:18:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: `hi. *\n\nNomoskar! Apuni ki kori ase?\nwhat are you doing?\n\nwhat are you doing? Moi apunak help kori roi asu as per my creator Suman's Order."`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
